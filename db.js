b1af4dddb0b8509a7a939f690d216c83f367366f
Muhammad Daniyal <daniyalwarsi12@gmail.com>
2023-05-03 19:20:48 +0500

    change db url

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const MongoURI = process.env.MONGO_URI
 
 ConnectToMongo = () => {
-  mongoose.connect(process.env.MONGO_URI);
+  mongoose.connect(MongoURI);
   const db = mongoose.connection;
   db.on("error", console.error.bind(console, "connection error:"));
   db.once("open", function () {
